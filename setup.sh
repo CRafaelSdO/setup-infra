438381849b6c593533c3a38caa635f7959e77980
C. Rafael Oliveira <crafaelsdo@gmail.com>
2026-05-15 02:38:23 -0300

    style: Correção de identação
    Corrige a identaão do arquivo 'setup.sh'.

--- setup.sh
+++ setup.sh
@@ -2,23 +2,23 @@
 
 # Verifica se o token foi passado como argumento
 if [ -z "$1" ]; then
-    echo "Erro: O CF_TUNNEL_TOKEN não foi fornecido."
-    echo "Uso: ./setup.sh <seu_token_aqui>"
-    exit 1
+  echo "Erro: O CF_TUNNEL_TOKEN não foi fornecido."
+  echo "Uso: ./setup.sh <seu_token_aqui>"
+  exit 1
 fi
 
 export CF_TUNNEL_TOKEN=$1
 
 # Função para criar rede se não existir
 create_network() {
-    NET_NAME=$1
+  NET_NAME=$1
 
-    if [ -z "$(docker network ls --filter name=^${NET_NAME}$ --format="{{.Name}}")" ]; then
-        echo "Criando rede docker: ${NET_NAME}..."
-        docker network create ${NET_NAME}
-    else
-        echo "Rede ${NET_NAME} já existe. Pulando..."
-    fi
+  if [ -z "$(docker network ls --filter name=^${NET_NAME}$ --format="{{.Name}}")" ]; then
+    echo "Criando rede docker: ${NET_NAME}..."
+    docker network create ${NET_NAME}
+  else
+    echo "Rede ${NET_NAME} já existe. Pulando..."
+  fi
 }
 
 echo "--- Iniciando Setup de Infraestrutura ---"
