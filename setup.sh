068a6b4db386293bab4cea535d6a9dac1335a886
Rafael Oliveira <crafaelsdo@gmail.com>
2026-06-06 08:25:29 -0300

    feat: setup.sh
    Ajusta o fluxo de execução e no 'setup.sh'.

--- setup.sh
+++ setup.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
 
 ENV_FILE=".env"
+EXAMPLE_FILE=".env.example"
 
-# Verifica se o token foi passado como argumento
+# Verifica se o token foi passado como argumento ou se já existe um .env
 if [ -n "$1" ]; then
-    echo "--- Token recebido via argumento. Salvando em $ENV_FILE ---"
+    echo "--- Token recebido via argumento. Salvando em '$ENV_FILE' ---"
 
     echo "CF_TUNNEL_TOKEN=$1" >"$ENV_FILE"
 
     chmod 600 "$ENV_FILE"
-elif [ -f "$ENV_FILE" ]; then
-    echo "--- Token não fornecido. Usando valores do arquivo $ENV_FILE ---"
+elif [ -f "$ENV_FILE" ] && grep -q "^CF_TUNNEL_TOKEN=" "$ENV_FILE"; then
+    echo "--- Token não fornecido. Usando valores do arquivo '$ENV_FILE' ---"
 else
-    echo "Erro: O CF_TUNNEL_TOKEN não foi fornecido."
-    echo "Uso: ./setup.sh <seu_token_aqui>"
+    echo "Erro: CF_TUNNEL_TOKEN não fornecido ou não presente no '$ENV_FILE'."
+    echo "Uso: ./setup.sh <seu_token_aqui> ou crie um '$ENV_FILE' seguindo o modelo de '$EXAMPLE_FILE'"
 
     exit 1
 fi
-
-set -a
-
-# shellcheck source=/dev/null
-source "$ENV_FILE"
-
-set +a
 
 # Função para criar rede se não existir
 create_network() {
     NET_NAME=$1
 
     if [ -z "$(docker network ls --filter name=^"${NET_NAME}"$ --format="{{.Name}}")" ]; then
-        echo "Criando rede docker: ${NET_NAME}..."
+        echo "Criando rede docker: '${NET_NAME}'..."
 
         docker network create "${NET_NAME}"
     else
-        echo "Rede ${NET_NAME} já existe. Pulando..."
+        echo "Rede '${NET_NAME}' já existe. Pulando..."
     fi
 }
 
